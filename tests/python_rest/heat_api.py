ade16780fe136ae9fb342e7d3cd641985e6116e0
jmhal <jmarcelo.alencar@gmail.com>
2016-04-06 16:17:39 -0300

    I was able to launch a stack from the API. Now I need to test sending a multiple file template.

--- tests/python_rest/heat_api.py
+++ tests/python_rest/heat_api.py
@@ -1,28 +1,69 @@
 #!/usr/bin/python
-# http://developer.openstack.org/api-guide/quick-start/api-quick-start.html
+# https://docs.saltstack.com/en/latest/topics/troubleshooting/yaml_idiosyncrasies.html
+import json
+import requests
+import os
+import yaml
+
 from os import environ as env
-import keystoneclient.v2_0.client as ksclient
-from heatclient.client import Client
+from os import path as path
+from datetime import datetime
+from dateutil import parser as parser
+from pytz import UTC as utc
 
-keystone = ksclient.Client(auth_url=env['OS_AUTH_URL'],
-                           username=env['OS_USERNAME'],
-                           password=env['OS_PASSWORD'],
-                           tenant_name=env['OS_TENANT_NAME'],
-                           region_name=env['OS_REGION_NAME'])
+def get_auth_token(url, tenant, username, password):
+   headers = {'Content-Type':'application/json'}
+   payload = {'auth':{'tenantName': tenant, 'passwordCredentials':{ 'username': username, 'password': password}}}
+   r = requests.post(url, data = json.dumps(payload), headers = headers)
+   token_id = r.json()['access']['token']['id']
+   return token_id
 
-heat_url = 'http://200.19.177.89:8004/v1/%s' % keystone.tenant_id
-heat = Client('1', endpoint=heat_url, token=keystone.auth_token)
+def get_tenant_id(url, token, tenant):
+   headers = {'X-Auth-Token': token}
+   r = requests.get(url, headers=headers)
+   json = r.json()
+   for element in json['tenants']:
+      if element['name'] == tenant:
+          return element['id']
 
-# keystone.tenant_id
-# stack_name = "api_cluster"
+def create_stack(url, token, tenant_id, stack_name, template_url):
+    headers = {'X-Auth-Token': token}
+    payload = {'tenant_id' : tenant_id, 'stack_name' : stack_name, 'template_url' : template_url,
+               'parameters' : {'image' : 'Ubuntu1404', 'flavor' : 'shelf.medium',
+               'key' : 'joaoalencar', 'private_network' : 'demo-net'}}
+    r = requests.post(url, data = json.dumps(payload), headers = headers)
+    print r.text
+    data = r.json()
+    return data['stack']['id']
+
+def create_stack(url, token, tenant_id, stack_name, template_file, files):
+    f = open(template_file, 'r')
+    template = yaml.load(f)
+    f.close()
+    headers = {'X-Auth-Token': token}
+    payload = {'tenant_id' : tenant_id, 'stack_name' : stack_name,
+               'template' : json.dumps(template),
+               'files' : '',
+               'parameters' : {'image' : 'Ubuntu1404', 'flavor' : 'shelf.medium',
+               'key' : 'joaoalencar', 'private_network' : 'demo-net'}}
+    r = requests.post(url, data = json.dumps(payload), headers = headers)
+    print r.status_code
+    print r.text
+    data = r.json()
+    return data['stack']['id']
+
+username = env['OS_USERNAME']
+password = env['OS_PASSWORD']
+tenant_name = env['OS_TENANT_NAME']
+
+token = get_auth_token(env['OS_AUTH_URL'] + "/tokens", tenant_name, username, password)
+tenant_id = get_tenant_id(env['OS_AUTH_URL'] + "/tenants", token, tenant_name)
+
+base_url = env['OS_AUTH_URL'].split(":")[1][2:]
+heat_base_url = "http://" + base_url + ":8004/v1"
+
 # template_url = "https://raw.githubusercontent.com/jmhal/infraservice/master/tests/heat_templates/heat_2a.yaml"
-parameters = "{'parameters':{ 'image' : 'Ubuntu1404', 'flavor' : 'hpcshelf.medium', 'key':'joaoalencar', 'private_network' : 'demonet'}}"
-
-
-fields = {
-	    'tenant_id' : keystone.tenant_id,
-            'stack_name': 'api_teste',
-            'parameters': parameters,
-            'template_url': 'https://raw.githubusercontent.com/jmhal/infraservice/master/tests/heat_templates/heat_2a.yaml'
-        }	
-heat.stacks.create(**fields)
+# create_stack(heat_base_url + "/" + tenant_id + "/stacks", token, tenant_id, "teste_api", template_url=template_url)
+template_file = "/home/jmhal/repositorios/infraservice/tests/heat_templates/heat_2a.yaml"
+files = {''}
+create_stack(heat_base_url + "/" + tenant_id + "/stacks", token, tenant_id, "teste_api_do_arquivo", template_file=template_file)
