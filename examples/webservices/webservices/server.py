b0156dfb1d99a8a07c6ba2bfeea7cb173d77e420
jmhal <jmhal@jupiter.cosmos>
2016-11-12 16:45:07 -0300

    Finally an example of a web service that knows the remote ip

--- examples/webservices/webservices/server.py
+++ examples/webservices/webservices/server.py
@@ -11,33 +11,25 @@ from inspect import getargspec
 server = SoapServer(
         service_name = 'Test',
         tns = 'http://example.com',
-        location = 'http://localhost:8000/api/',
-#        location = 'http://192.168.1.110:8000/api',
+#        location = 'http://localhost:8000/api/',
+        location = 'http://192.168.1.110:8000/api/',
 )
 
-"""
-@register('add_simple',needs_context=True)
-def add_simple(a, b, context):
-   print "The Remote IP is", context
-   return a + b
-
-
-f = NativeFunctionAdapter(add_simple)
-server.add_function(f)
-"""
+@register()
+def add_triple(a, b, c):
+   return a + b + c
 
 class Add_Simple(Function):
    def __init__(self):
       self.remote_ip = None
       self.name = "add_simple"
-      self.documentation = "who cares?"
+      self.documentation = "a simple add/concatenate function"
       self.return_type = TypeFactory(str)
       self.needs_context = False 
       args_ = [('a', str), ('b', str)]
       self.args = DictOf("add_simple", *args_)
 
    def call(self, **args):
-      print args
       return self.add_simple(**args)
 
    def add_simple(self, a, b):
@@ -45,12 +37,5 @@ class Add_Simple(Function):
       return a + b
 
 f  = Add_Simple()
-
-# print len(f.args.fields)
-# field0 = f.args.fields[0]
-# print field0.name
-# print field0.type
-# print f.return_type
-
 server.add_function(f)
 
