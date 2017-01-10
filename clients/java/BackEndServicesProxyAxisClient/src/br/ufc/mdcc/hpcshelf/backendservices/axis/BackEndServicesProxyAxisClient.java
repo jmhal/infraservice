b125029a1b09d6a27626bd8b7310e60815fc0a70
jmhal <jmarcelo.alencar@gmail.com>
2017-01-10 11:03:05 -0300

    Trying to create a .gitignore.

--- clients/java/BackEndServicesProxyAxisClient/src/br/ufc/mdcc/hpcshelf/backendservices/axis/BackEndServicesProxyAxisClient.java
+++ clients/java/BackEndServicesProxyAxisClient/src/br/ufc/mdcc/hpcshelf/backendservices/axis/BackEndServicesProxyAxisClient.java
@@ -8,6 +8,9 @@ import br.ufc.mdcc.hpcshelf.backendservices.proxy.BackEndServicesImplServiceStub
 import br.ufc.mdcc.hpcshelf.backendservices.proxy.DeployContractCallbackDocument;
 import br.ufc.mdcc.hpcshelf.backendservices.proxy.DeployContractCallbackDocument.DeployContractCallback;
 import br.ufc.mdcc.hpcshelf.backendservices.proxy.DeployContractCallbackResponseDocument;
+import br.ufc.mdcc.hpcshelf.backendservices.proxy.DestroyPlatformDocument;
+import br.ufc.mdcc.hpcshelf.backendservices.proxy.DestroyPlatformDocument.DestroyPlatform;
+import br.ufc.mdcc.hpcshelf.backendservices.proxy.DestroyPlatformResponseDocument;
 
 public class BackEndServicesProxyAxisClient {
 
@@ -15,9 +18,8 @@ public class BackEndServicesProxyAxisClient {
 		// TODO Auto-generated method stub
 		try {
 			BackEndServicesImplServiceStub stub = new BackEndServicesImplServiceStub(args[0]);
-			
+			/* Create Platform
 			DeployContractCallbackDocument request = DeployContractCallbackDocument.Factory.newInstance();
-			
 			DeployContractCallback data = request.addNewDeployContractCallback();
 			
 			data.setArg0("321");
@@ -25,6 +27,19 @@ public class BackEndServicesProxyAxisClient {
 			
 			DeployContractCallbackResponseDocument response = stub.deployContractCallback(request);
 			System.out.println(response.getDeployContractCallbackResponse().getReturn());
+			
+            // Wait for a few second just to be sure
+			Thread.sleep(60000);
+			*/
+			// Destroy Platform
+			DestroyPlatformDocument destroyRequest = DestroyPlatformDocument.Factory.newInstance();
+			DestroyPlatform destroyData = destroyRequest.addNewDestroyPlatform();
+			
+			destroyData.setArg0("185");
+			
+			DestroyPlatformResponseDocument destroyResponse = stub.destroyPlatform(destroyRequest);
+			System.out.println(destroyResponse.getDestroyPlatformResponse().getReturn());
+			
 		} catch (AxisFault e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
